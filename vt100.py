cead330c2eaf398ce820728538690398cecc4d02
Antonio Dell'Elce <antouk@dellelce.com>
2016-07-23 17:13:08 +0100

    minor improvements / corrections

--- vt100.py
+++ vt100.py
@@ -4,9 +4,8 @@
 # Created:      1346 100713
 # Description:  class to handle vt100 terminal configuration & output in a pythonic way
 #
-#
 
-class vt100:
+class vt100(object):
 
   esc = chr(27);
 
@@ -23,21 +22,20 @@ class vt100:
 
     return self.esc + '];' + name + chr(7)
 
-
   @classmethod
   def cursor_pos(self, x = 0, y = 0):
 
-     try:
-       x_s = str(x)
-     except: 
-       x_s = '0'
+    try:
+      x_s = str(x)
+    except: 
+      x_s = '0'
 
-     try:
-       y_s = str(y)
-     except: 
-       y_s = '0'
+    try:
+      y_s = str(y)
+    except: 
+      y_s = '0'
 
-     return self.esc + '[' + x_s + ';' + y_s + 'H'
+    return self.esc + '[' + x_s + ';' + y_s + 'H'
 
 ## EOF ##
 
