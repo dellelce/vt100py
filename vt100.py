e141d695252ec10e7e50f87fe845d3013943996c
Antonio Dell'Elce <antouk@dellelce.com>
2017-12-26 17:31:02 +0100

    Identation update

--- vt100.py
+++ vt100.py
@@ -1,41 +1,42 @@
 #!/usr/bin/env python
 #
 # File:         vt100.py
-# Created:      1346 100713
+# Created:      100713
 # Description:  class to handle vt100 terminal configuration & output in a pythonic way
 #
 
 class vt100(object):
+ '''vt100'''
 
-  esc = chr(27);
+ esc = chr(27);
 
-  @classmethod
-  def bold(self):
-    return self.esc + '[1m'
+ @classmethod
+ def bold(self):
+  return self.esc + '[1m'
 
-  @classmethod
-  def reset(self):
-    return self.esc + '[0m'
+ @classmethod
+ def reset(self):
+  return self.esc + '[0m'
 
-  @classmethod
-  def title(self, name = None):
+ @classmethod
+ def title(self, name = None):
 
-    return self.esc + '];' + name + chr(7)
+  return self.esc + '];' + name + chr(7)
 
-  @classmethod
-  def cursor_pos(self, x = 0, y = 0):
+ @classmethod
+ def cursor_pos(self, x = 0, y = 0):
 
-    try:
-      x_s = str(x)
-    except: 
-      x_s = '0'
+  try:
+   x_s = str(x)
+  except:
+   x_s = '0'
 
-    try:
-      y_s = str(y)
-    except: 
-      y_s = '0'
+  try:
+   y_s = str(y)
+  except:
+   y_s = '0'
 
-    return self.esc + '[' + x_s + ';' + y_s + 'H'
+  return self.esc + '[' + x_s + ';' + y_s + 'H'
 
 ## EOF ##
 
